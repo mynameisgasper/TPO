289f028965606c9b6dfcb7b8965251babb39466a
mynameisgasper <stepec.gasper97@gmail.com>
2021-05-06 11:02:41 +0200

    nove komponente

---
+++ src/app_public/src/app/vsi-oglasi/vsi-oglasi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vsi-oglasi',
+  templateUrl: './vsi-oglasi.component.html',
+  styleUrls: ['./vsi-oglasi.component.css']
+})
+export class VsiOglasiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
