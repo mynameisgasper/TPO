4a0397d1e9866421da59020c37ae35efc9c99c8d
Grega Kranjec <grega.kranjec@outlook.com>
2021-05-07 17:55:48 +0200

    Brisanje komentarja omejitev na lastnike in admine #10

--- src/app_api/controllers/user.js
+++ src/app_api/controllers/user.js
@@ -7,16 +7,13 @@ const getAll = (req, res) => {
     User
         .find()
         .exec((napaka, users) => {
-            if(napaka) {
+            if (napaka) {
                 return res.status(500).json(napaka);
-            }
-            else if(!users){
+            } else if (!users) {
                 return res.status(404).json({
-                    "sporocilo":
-                    "Ne najdem podatkov"
+                    "sporocilo": "Ne najdem podatkov"
                 });
-            }
-            else {
+            } else {
 
                 res.status(200).json(users);
             }
@@ -27,18 +24,16 @@ const getOne = (req, res) => {
     User
         .findById(req.params.id)
         .exec((napaka, user) => {
-           if(!user){
-               return res.status(404).json({
-                   "sporocilo":
-                       "Ne najdem uporabnika"
-               });
-           }
-           else if (napaka){
-               return res.status(500).json(napaka,
-                "api napaka");
-           }
+            if (!user) {
+                return res.status(404).json({
+                    "sporocilo": "Ne najdem uporabnika"
+                });
+            } else if (napaka) {
+                return res.status(500).json(napaka,
+                    "api napaka");
+            }
 
-           res.status(200).json(user);
+            res.status(200).json(user);
         });
 
 
@@ -53,12 +48,12 @@ const create = (req, res) => {
         !req.body.role ||
         !req.body.country ||
         !req.body.password) {
-        res.status(400).json({"sporočilo": "Zahtevani so vsi podatki"});
-    }else{
-        if(req.payload.role===2){
+        res.status(400).json({ "sporočilo": "Zahtevani so vsi podatki" });
+    } else {
+        if (req.payload.role === 2) {
             const uporabnik = new User();
             //nastavimo vse podatke za uporabnika
-            uporabnik.name =  req.body.name
+            uporabnik.name = req.body.name
             uporabnik.surname = req.body.surname
             uporabnik.email = req.body.email
             uporabnik.phone = req.body.phone
@@ -74,34 +69,34 @@ const create = (req, res) => {
                     res.status(200).json(true);
                 }
             });
-        }else{
+        } else {
             res.status(403).json({
-                message:'dostop do te funckionalnosti je prepovedan za neadministratorske uporabnike!'
+                message: 'dostop do te funckionalnosti je prepovedan za neadministratorske uporabnike!'
             })
         }
     }
 };
 
 const update = (req, res) => {
-    let user = {...req.body}
+    let user = {...req.body }
     console.log();
-    User.updateOne({_id:req.params.id}, user, (err)=>{
-        if(err){
+    User.updateOne({ _id: req.params.id }, user, (err) => {
+        if (err) {
             console.log(err);
             res.status(500);
-        }else {
+        } else {
             res.status(200).json(user);
         }
     })
 };
 
 const deleteOne = (req, res) => {
-    if(req.payload.role!==2){
+    if (req.payload.role !== 2) {
         return res.status(403).json({
-            message:'dostop do te funckionalnosti je prepovedan za neadministratorske uporabnike!'
+            message: 'dostop do te funckionalnosti je prepovedan za neadministratorske uporabnike!'
         })
     } else {
-        if(req.params.id){
+        if (req.params.id) {
             User
                 .findByIdAndDelete(req.params.id)
                 .exec((napaka) => {
@@ -110,91 +105,111 @@ const deleteOne = (req, res) => {
                     }
                     res.status(204).json(null);
                 });
-        }
-        else {
+        } else {
             return res.status(404).json({
-                "sporocilo":
-                    "Ne najdem uporabnika"
+                "sporocilo": "Ne najdem uporabnika"
             });
         }
     }
 };
 
 // todo test
-const addComment = (req,res) => {
-        User.findById(req.query.userId).exec().then(user=>{
-            if(!user){
-                return res.status(404).json({"sporocilo": "Ne najdem uporabnika"});
-            }else{
-                const comment = new Comment({
-                    owner: req.payload.email,
-                    content: req.body.content
-                })
-                user.comments.push(comment)
-                user.save((err, doc)=>{
-                    if(err){
-                        return res.status(500).json({"message":"internal server error"})
-                    }else{
+const addComment = (req, res) => {
+    User.findById(req.query.userId).exec().then(user => {
+        if (!user) {
+            return res.status(404).json({ "sporocilo": "Ne najdem uporabnika" });
+        } else {
+            const comment = new Comment({
+                owner: req.payload.email,
+                content: req.body.content
+            })
+            user.comments.push(comment)
+            user.save((err, doc) => {
+                if (err) {
+                    return res.status(500).json({ "message": "internal server error" })
+                } else {
+                    console.log(doc)
+                    return res.status(200).json({ "messge": "komentar uspešno dodan" })
+                }
+            })
+        }
+    }).catch(err => {
+        console.error(err.message)
+        return res.status(500).json({ "message": "internal server error" })
+    })
+}
+
+
+const deleteComment = (req, res) => {
+    User.findById(req.query.userId).exec().then(user => {
+        if (!user) {
+            return res.status(404).json({ "sporocilo": "Ne najdem uporabnika" });
+        } else {
+            if (req.payload.role == 2) {
+                user.comments.splice(user.comments.find(c => c._id === req.query.commentId && c.owner === req.payload.email), 1)
+                user.save((err, doc) => {
+                    if (err) {
+                        return res.status(500).json({ "message": "internal server error" })
+                    } else {
                         console.log(doc)
-                        return res.status(200).json({"messge":"komentar uspešno dodan"})
+                        return res.status(200).json({ "message": "komentar uspešno izbrisan" })
                     }
                 })
             }
-        }).catch(err=>{
-            console.error(err.message)
-            return res.status(500).json({"message":"internal server error"})
-        })
-}
-
-
-const deleteComment = (req,res) => {
-    User.findById(req.query.userId).exec().then(user=>{
-        if(!user){
-            return res.status(404).json({"sporocilo": "Ne najdem uporabnika"});
-        }else{
-            user.comments.splice(user.comments.find(c => c._id === req.query.commentId && c.owner === req.payload.email), 1)
-            user.save((err, doc)=>{
-                if(err){
-                    return res.status(500).json({"message":"internal server error"})
-                }else{
-                    console.log(doc)
-                    return res.status(200).json({"message":"komentar uspešno izbrisan"})
+            else {
+                for(var i = 0; i < user.comments.length; i++){
+                    if(user.comments[i]._id == req.query.commentId){
+                        if(user.comments[i].owner === req.payload.email) {
+                            user.comments.splice(user.comments.find(c => c._id === req.query.commentId && c.owner === req.payload.email), 1)
+                            user.save((err, doc) => {
+                                if (err) {
+                                    return res.status(500).json({ "message": "internal server error" })
+                                } else {
+                                    console.log(doc)
+                                    return res.status(200).json({ "message": "komentar uspešno izbrisan" })
+                                }
+                            })
+                        } else {
+                            return res.status(401).json({"message": "uporabnik nima pravice brisati ta komentar"})
+                        }
+                    }
                 }
-            })
+            }
         }
-    }).catch(err=>{
+    }).catch(err => {
         console.error(err.message)
-        return res.status(500).json({"message":"internal server error"})
+        return res.status(500).json({ "message": "internal server error" })
     })
+
 }
 
-const addRating = (req,res) => {
-    User.findById(req.query.userId).exec().then(user=>{
-        if(!user){
-            return res.status(404).json({"sporocilo": "Ne najdem uporabnika"});
-        } else if(user.ratingsFrom.includes(rec.payload.email)) {
-            return res.status(400).json({"sporocilo": "Temu uporabniku ste ze podali oceno"});
-        }else {
+const addRating = (req, res) => {
+    User.findById(req.query.userId).exec().then(user => {
+        if (!user) {
+            return res.status(404).json({ "sporocilo": "Ne najdem uporabnika" });
+        } else if (user.ratingsFrom.includes(rec.payload.email)) {
+            return res.status(400).json({ "sporocilo": "Temu uporabniku ste ze podali oceno" });
+        } else {
             user.ratingSum += req.body.rating
             user.ratingNum++
-            user.rating = user.ratingSum / user.ratingNum 
+                user.rating = user.ratingSum / user.ratingNum
             user.ratingsFrom.push(req.payload.email)
-            user.save((err, doc)=>{
-                if(err){
-                    return res.status(500).json({"message":"internal server error"})
-                }else{
+            user.save((err, doc) => {
+                if (err) {
+                    return res.status(500).json({ "message": "internal server error" })
+                } else {
                     console.log(doc)
-                    return res.status(200).json({"messge":"ocena uspešno dodana"})
+                    return res.status(200).json({ "messge": "ocena uspešno dodana" })
                 }
             })
         }
-    }).catch(err=>{
+    }).catch(err => {
         console.error(err.message)
-        return res.status(500).json({"message":"internal server error"})
+        return res.status(500).json({ "message": "internal server error" })
     })
 }
 
-const getAllOglasi = (req,res) => {
+const getAllOglasi = (req, res) => {
     //todo pridobi vse oglase uporabnika in vrni
 }
 
