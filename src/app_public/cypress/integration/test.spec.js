f5edede1e6269ee10a4810d5a08c1fccef1f74fd
mynameisgasper <stepec.gasper97@gmail.com>
2021-05-07 15:50:19 +0200

    Ogled oglasa test #12

--- src/app_public/cypress/integration/test.spec.js
+++ src/app_public/cypress/integration/test.spec.js
@@ -1,5 +1,4 @@
 import Chance from 'chance';
-import { TimeoutError } from 'cypress/types/bluebird';
 const chance = new Chance();
 
 describe('Testiranje Dog walkers', () => {
@@ -12,12 +11,14 @@ describe('Testiranje Dog walkers', () => {
         TEST REGISTRACIJE
     */
 
-    /*context('Ogled vseh oglasov', () => {
+    context('Ogled vseh oglasov', () => {
          it('Ogled vseh oglasov', () => {
             cy.visit('http://localhost:4200')
             cy.get('.guest').click()
+            cy.wait(100)
+            cy.get('#ustvari').should('have.text','Ustvari oglas')
         })
-    })*/
+    })
     
     context('Vse od prijave naprej', () => {
         it('Prijava uporabnika', () => {
@@ -37,6 +38,17 @@ describe('Testiranje Dog walkers', () => {
 
             //Počakaj klic
             cy.wait('@login')
+            
+            //Preveri če obstaja gumb Odjava -> Tako vemo da je prijavljen
+            cy.get('#odjava').should('have.text','Odjava')
+        })
+
+        it('Ogled oglasa', () => {
+            
+            cy.get('#oglasButton').first().click()
+            cy.wait(100)
+            cy.get('#odziv').should('have.text','Odziv na oglas')
+
         })
     })
 
