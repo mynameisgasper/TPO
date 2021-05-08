46188ae6cf12ce4e74ad242a4a40d0ba210675e8
mavress <61246527+mavress@users.noreply.github.com>
2021-05-09 01:01:36 +0200

    delo na testih #12

--- src/test/test.spec.js
+++ src/test/test.spec.js
@@ -1,37 +1,71 @@
 describe('Testiranje Dog walkers', () => {
+
+    beforeEach(() => {
+        cy.visit('http://localhost:4200/')
+    })
+
+    Cypress.Commands.add('login', () => { 
+            
+        //Pripravi API klic ki ga želimo prestreči in počakati
+        cy.intercept('POST', 'http://localhost:3000/api/v1/auth/login').as('login');
+        
+        cy.contains('DOG WALKERS')
+        //Vnesi v polja in klikni na gumb
+        cy.get('#loginEmail')
+        .type('marica.petkovsek@hotmail.com').should('have.value', 'marica.petkovsek@hotmail.com')
+        cy.get('#loginPassword')
+        .type('test')
+        cy.get('#login').click()
+
+        //Počakaj klic
+        cy.wait('@login')
+    })
+
+    Cypress.Commands.add('loginAdmin', () => { 
+
+        //Pripravi API klic ki ga želimo prestreči in počakati
+        cy.intercept('POST', 'http://localhost:3000/api/v1/auth/login').as('login');
+        
+        cy.contains('DOG WALKERS')
+        //Vnesi v polja in klikni na gumb
+        cy.get('#loginEmail')
+        .type('ad.min@hotmail.com').should('have.value', 'ad.min@hotmail.com')
+        cy.get('#loginPassword')
+        .type('admin')
+        cy.get('#login').click()
+
+        //Počakaj klic
+        cy.wait('@login')
+    })
     
-    /*beforeEach(() => {
-        cy.visit('http://localhost:4200')
-    })*/
     
     /*
         TODO:
         -----
-            PRIJAVA UPORABNIKA: 0
-            REGISTRACIJA UPORABNIKA: 2
-            PREGLED VSEH OGLASOV: 1
-            OGLED POSAMEZNEGA OGLASA: 1
-            ISKANJE OGLASOV: 2
-            UREJANJE UPORABNIŠKEGA PROFILA: 2
-            OGLED PROFILA: 2
-            KREIRANJE OGLASA: 2
-            UREJANJE OGLASA: 2
-            BRISANJE OGLASA: 2
-            ODZIV NA OGLAS: 2
-            OCENA PROFILA: 2
-            KOMENTIRANJE PROFILA: 2
-            DODAJANJE UPORABNIKA MED HITRE KONTAKTE: 2
-            OGLED HITRIH KONTAKTOV: 2
-            ODSTRANITEV IZ HITRIH KONTAKTOV: 2
-            PREGLED LOKACIJE PREVZEMA: 2
-            PRETVORBA VALUTE: 2
+        PRIJAVA UPORABNIKA: 0
+        REGISTRACIJA UPORABNIKA: 2
+        PREGLED VSEH OGLASOV: 1
+        OGLED POSAMEZNEGA OGLASA: 1
+        ISKANJE OGLASOV: 2
+        UREJANJE UPORABNIŠKEGA PROFILA: 2
+        OGLED PROFILA: 2
+        KREIRANJE OGLASA: 2
+        UREJANJE OGLASA: 2
+        BRISANJE OGLASA: 2
+        ODZIV NA OGLAS: 2
+        OCENA PROFILA: 2
+        KOMENTIRANJE PROFILA: 2
+        DODAJANJE UPORABNIKA MED HITRE KONTAKTE: 2
+        OGLED HITRIH KONTAKTOV: 2
+        ODSTRANITEV IZ HITRIH KONTAKTOV: 2
+        PREGLED LOKACIJE PREVZEMA: 2
+        PRETVORBA VALUTE: 2
     */
 
     // TESTI: PREGLED VSEH OGLASOV
     context('Pregled vseh oglasov', () => {
 
-        it('Ogled vseh oglasov', () => {
-            cy.visit('http://localhost:4200')
+        it('Pregled vseh oglasov', () => {
             cy.get('.guest').click()
             cy.wait(100)
             cy.get('#ustvari').should('have.text','Ustvari oglas')
@@ -42,8 +76,6 @@ describe('Testiranje Dog walkers', () => {
     context('Prijava uporabnika', () => {
 
         it('Neuspešna prijava uporabnika', () => {
-            //Pojdi na začetno stran
-            cy.visit('http://localhost:4200')
             
             //Pripravi API klic ki ga želimo prestreči in počakati
             cy.intercept('POST', 'http://localhost:3000/api/v1/auth/login').as('login');
@@ -59,48 +91,27 @@ describe('Testiranje Dog walkers', () => {
             //alert
             cy.on('window:alert', (str) => {
                 expect(str).to.equal('neuspešna prijava, glej konzolo')
-              })
+            })
         })
 
         it('Uspešna prijava uporabnika', () => {
-            //Pojdi na začetno stran
-            cy.visit('http://localhost:4200')
-            
-            //Pripravi API klic ki ga želimo prestreči in počakati
-            cy.intercept('POST', 'http://localhost:3000/api/v1/auth/login').as('login');
-            
-            cy.contains('DOG WALKERS')
-            //Vnesi v polja in klikni na gumb
-            cy.get('#loginEmail')
-            .type('marica.petkovsek@hotmail.com').should('have.value', 'marica.petkovsek@hotmail.com')
-            cy.get('#loginPassword')
-            .type('test')
-            cy.get('#login').click()
 
-            //Počakaj klic
-            cy.wait('@login')
-            
+            cy.login()
+
             //Preveri če obstaja gumb Odjava -> Tako vemo da je prijavljen
             cy.get('#odjava').should('have.text','Odjava')
+
+            cy.setCookie('testni_kuks', 'kukers')
         })
     })
-        
-
-        // TEST: OGLED POSAMEZNEGA OGLASA #2
-        
-
-        
-        // TEST: OGLED PROFILA #2
-
-        // TEST: KREIRANJE OGLASA #1
-
-        // TEST: KREIRANJE OGLASA #2
-
     
     // TESTI: OGLED POSAMEZNEGA OGLASA
     context('Ogled posameznega oglasa', () => {
 
         it('Ogled oglasa', () => {
+
+            //Pojdi na stran z vsemi oglasi
+            cy.visit('http://localhost:4200/oglasi')
             
             cy.get('#oglasButton').first().click()
             cy.wait(100)
@@ -115,68 +126,89 @@ describe('Testiranje Dog walkers', () => {
         */
     })
 
-    /*
-    context('Od prijave naprej', () => {
-
-        // TEST: PRIJAVA UPORABNIKA #2
-        it('Uspešna prijava uporabnika', () => {
-            //Pojdi na začetno stran
-            cy.visit('http://localhost:4200')
-            
-            //Pripravi API klic ki ga želimo prestreči in počakati
-            cy.intercept('POST', 'http://localhost:3000/api/v1/auth/login').as('login');
-            
-            cy.contains('DOG WALKERS')
-            //Vnesi v polja in klikni na gumb
-            cy.get('#loginEmail')
-            .type('marica.petkovsek@hotmail.com').should('have.value', 'marica.petkovsek@hotmail.com')
-            cy.get('#loginPassword')
-            .type('test')
-            cy.get('#login').click()
-
-            //Počakaj klic
-            cy.wait('@login')
-            
-            //Preveri če obstaja gumb Odjava -> Tako vemo da je prijavljen
-            cy.get('#odjava').should('have.text','Odjava')
-        })
+    // TESTI: UREJANJE UPORABNIŠKEGA PROFILA
+    context('Urejanje uporabniškega profila', () => {
         
-        // TEST: UREJANJE UPORABNIŠKEGA PROFILA #1
         it('Urejanje profila uporabnika', () => {
 
-            //Pojdi na začetno stran
-            cy.visit('http://localhost:4200')
-            
-            //Pripravi API klic ki ga želimo prestreči in počakati
-            cy.intercept('POST', 'http://localhost:3000/api/v1/auth/login').as('login');
-            
-            cy.contains('DOG WALKERS')
-            //Vnesi v polja in klikni na gumb
-            cy.get('#loginEmail')
-            .type('marica.petkovsek@hotmail.com').should('have.value', 'marica.petkovsek@hotmail.com')
-            cy.get('#loginPassword')
-            .type('test')
-            cy.get('#login').click()
-
-            //Počakaj klic
-            cy.wait('@login')
+            cy.login()
 
             cy.get('#userProfileButton').click()
             cy.wait(100)
             cy.get('#editButtonProfile').click()
+
+            //Preveri ali si na strani za urejanje profila
             cy.get('.naslov').should('have.text','Urejanje profila')
         })
-        
-        // TEST: UREJANJE UPORABNIŠKEGA PROFILA #2
 
-        // TEST: OGLED PROFILA #1
+    })
+
+    // TESTI: OGLED PROFILA
+    context('Ogled profila', () => {
+
         it('Ogled lastnega profila', () => {
+
+            cy.login()
+
             cy.get('#userProfileButton').click()
             cy.wait(100)
             cy.get('.naslov').should('have.text','Profil uporabnika')
         })
+
+        // TODO, ko bo zvezano
+        /*
+        it('Ogled profila drugega uporabnika', () => {
+        })
+        */
     })
-    */
+    
+    // TESTI: KREIRANJE OGLASA
+    context('Kreiranje oglasa', () => {
+
+        /*
+        it('Kreiranje oglasa uspešen', () => {
+
+            cy.login()
+
+            cy.get('#ustvari').click()
+            cy.wait(50)
+
+            // Kreator novega oglasa vnese podatke
+
+            let n = "testni_oglas"
+
+            cy.get('#naslovOglasa')
+            .type(n)
+            cy.get('#opisOglasa')
+            .type('test 123 123')
+            cy.get('#cenaOglasa')
+            .type('69')
+
+            // Kreator oglasa doda sliko iz lokalne naprave
+            // cy.get('#dodajSliko').click()
+
+            // Kreator doda nov oglas k obstoječim
+            cy.get('#buttonAddOglas').click()
+            cy.wait(50)
+
+            cy.get('#nasOglasaBoard').should('have.text', n)
+        })*/
+
+        it('Kreiranje oglasa neuspešen', () => {
+
+            cy.login()
+
+            cy.get('#ustvari').click()
+            cy.wait(50)
+            cy.get('#buttonAddOglas').click()
+            
+            //alert
+            cy.on('window:alert', (str) => {
+                expect(str).to.equal('Napaka pri kreaciji oglasa, glej konzolo!')
+            })
+            
+        })
+    })
 
     
 })
